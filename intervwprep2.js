63b79f4b94d411bcaede1833447ef1350ffcb123
deepika-37 <107699081+deepika-37@users.noreply.github.com>
2022-09-10 15:47:27 +0530

    Add files via upload

---
+++ intervwprep2.js
@@ -0,0 +1,6 @@
+explain call(),apply(),blind() methods.give an examples of call(),apply(),blind().
+call(): call method invokes a function.these are just different invoking  or calling a function. 
+when you are calling a function using any of these 3 then you don;t have to define in the function that it will be taking as a parameter.
+and it will assume that the 1st argument. 
+in case of call each and every argument which is required to passed to the function will be individually.
+example: 
