353d196ba28ce58ed842713c396823c05a99d153
deepika-37 <107699081+deepika-37@users.noreply.github.com>
2022-09-13 15:04:32 +0530

    Add files via upload

---
+++ interviewprep3.js
@@ -0,0 +1,22 @@
+//1)what are promises and why do we need them ?
+
+Promises are the ideal choice for handling asynchronous operations in the simplest manner. They can handle multiple asynchronous operations easily and provide better error handling than callbacks and events. A promise can be created using Promise constructor. Promise constructor takes only one argument,a callback function.
+
+promises are used to handle asynchronous operations donot create callback hells.
+In promises there are 3 stages pending ,resolved,rejection.
+
+2)what is the purpose of async and await keywords?
+
+
+
+
+what is hoisting?
+
+
+
+
+what is the DOM?
+
+   
+
+
